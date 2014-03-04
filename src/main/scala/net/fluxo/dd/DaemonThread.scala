4e2a128b0ec0348404134deb741a16070cd68158
Ronald Kurniawan (viper) <r.kurniawan@fluxodesign.net>
2014-03-04 23:00:29 +1000

    * DaemonThread.scala: added isTorrentDirExists()

--- src/main/scala/net/fluxo/dd/DaemonThread.scala
+++ src/main/scala/net/fluxo/dd/DaemonThread.scala
@@ -1,6 +1,7 @@
 package net.fluxo.dd
 
 import org.apache.log4j.Level
+import java.io.File
 
 /**
  * User: Ronald Kurniawan (viper)
@@ -17,6 +18,14 @@ class DaemonThread extends Thread {
 		LogWriter.writeLog("Daemon started on " + LogWriter.currentDateTime, Level.INFO)
 		if (_isRunning) {
 			// we need to check the ./torrents directory to see if there is any new ones to download...
+			if (isTorrentDirExists) {
+
+			}
 		}
+	}
+
+	def isTorrentDirExists: Boolean = {
+		val dir = new File("./torrents")
+		dir.exists() && dir.isDirectory && dir.canRead
 	}
 }
