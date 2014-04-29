a82f5ebb854188533dfbe9646d0f1c51ecead87c
Ronald Kurniawan (viper) <r.kurniawan@fluxodesign.net>
2014-04-29 20:10:53 +1000

    STILL DEBUGGING THE DAMN THING!
    WE ARE NOW USING APACHE COMMONS EXEC

--- src/main/scala/net/fluxo/dd/AriaProcessor.scala
+++ src/main/scala/net/fluxo/dd/AriaProcessor.scala
@@ -41,11 +41,12 @@ class AriaProcessor {
 		if (rpcPort < 0) return "All download slots taken, try again later"
 		var newGid = OUtils.generateGID()
 		while (DbControl.isTaskGIDUsed(newGid)) newGid = OUtils.generateGID()
-		val ariaThread = new AriaThread(rpcPort, uri, newGid, owner, false, isHttp)
+		val ariaThread = new AriaThread(rpcPort, uri, newGid, /*owner, false,*/ isHttp)
 		if (httpUsername.length > 0 && httpPassword.length > 0) {
 			ariaThread.setCredentials(httpUsername, httpPassword)
 		}
 		new Thread(ariaThread).start()
+		stat(rpcPort, restarting = false, newGid, owner, uri, isHttp = isHttp, httpUsername, httpPassword, ariaThread getExecutor())
 		"OK " + newGid
 	}
 
@@ -107,23 +108,102 @@ class AriaProcessor {
 				return
 			}
 			val ariaThread = new AriaThread(rpcPort, t.TaskInput.getOrElse(null), t.TaskGID.getOrElse(null),
-				t.TaskOwner.getOrElse(null), true, t.TaskIsHttp)
+				/*t.TaskOwner.getOrElse(null), true,*/ t.TaskIsHttp)
 			if (t.TaskIsHttp) {
 				if (t.TaskHttpUsername.getOrElse("").length > 0 && t.TaskHttpPassword.getOrElse("").length > 0) {
 					ariaThread.setCredentials(t.TaskHttpUsername.getOrElse(""), t.TaskHttpPassword.getOrElse(""))
 				}
 			}
 			new Thread(ariaThread).start()
+			stat(rpcPort, restarting = true, t.TaskGID.getOrElse(""), t.TaskOwner.getOrElse(""), t.TaskInput.getOrElse(""),
+				isHttp = t.TaskIsHttp, t.TaskHttpUsername.getOrElse(""), t.TaskHttpPassword.getOrElse(""), ariaThread getExecutor)
 		}
 	}
 
-	class AriaThread(port: Int, uri: String, gid: String, owner: String, restarting: Boolean, isHttp: Boolean) extends Runnable {
+	def stat(port:Int, restarting: Boolean, gid: String, owner: String, uri: String, isHttp: Boolean,
+	    httpUsername: String, httpPassword: String, executor: DefaultExecutor) {
+
+		// we sleep for 3s, to allow the newly started process to settle...
+		try { Thread sleep 3000 } catch { case ie: InterruptedException => }
+		if (!restarting) {
+			// DEBUG
+			LogWriter writeLog ("Adding new task to DB", Level.DEBUG)
+			DbControl.addTask(new Task {
+				TaskGID_=(gid)
+				TaskInput_=(uri)
+				TaskOwner_=(owner)
+				TaskStarted_=(DateTime.now().getMillis)
+				TaskIsHttp_=(isHttp)
+				if (httpUsername.length > 0 && httpPassword.length > 0) {
+					TaskHttpUsername_=(httpUsername)
+					TaskHttpPassword_=(httpPassword)
+				}
+			})
+		}
+		// DEBUG
+		LogWriter writeLog ("Adding new active task to list...", Level.DEBUG)
+		ActiveProcesses.add(new AriaProcess {
+			AriaPort_=(port)
+			//AriaProcess_=(process)
+			AriaProcess_:(executor)
+			AriaTaskGid_=(gid)
+			AriaTaskRestarting_=(restarting)
+			AriaHttpDownload_=(isHttp)
+			//AriaTaskPID_=(taskPid)
+		})
+		// set all necessary parameters if this is an HTTP download...
+		if (isHttp) {
+			DbControl.updateTaskTailGID(gid, gid)
+			try {
+				TimeUnit.SECONDS.sleep(5)
+			} catch {
+				case ie: InterruptedException =>
+			}
+			val rpcClient = OUtils.getXmlRpcClient(port)
+			val active = OUtils.sendAriaTellActive(rpcClient)
+			if (active.length > 0) {
+				for (o <- active) {
+					val jMap = o.asInstanceOf[java.util.HashMap[String, Object]]
+					val tailGID = OUtils.extractValueFromHashMap(jMap, "gid").toString
+					val task = {
+						if (tailGID.length > 0) DbControl.queryTaskTailGID(tailGID) else null
+					}
+					val cl = OUtils.extractValueFromHashMap(jMap, "completedLength").toString.toLong
+					task.TaskCompletedLength_=(cl)
+					val tl = OUtils.extractValueFromHashMap(jMap, "totalLength").toString.toLong
+					task.TaskTotalLength_=(tl)
+					task.TaskStatus_=(OUtils.extractValueFromHashMap(jMap, "status").toString)
+					task.TaskInfoHash_=("noinfohash")
+					// now we extract the 'PACKAGE' name, which basically is the name of the directory of the downloaded files...
+					val objFiles = OUtils.extractValueFromHashMap(jMap, "files").asInstanceOf[Array[Object]]
+					if (objFiles.length > 0) {
+						val files = objFiles(0).asInstanceOf[java.util.HashMap[String, Object]]
+						val path = OUtils.extractValueFromHashMap(files, "path").asInstanceOf[String]
+						task.TaskPackage_=(FilenameUtils.getName(path))
+					}
+					val progress = (task.TaskCompletedLength * 100)/task.TaskTotalLength
+					LogWriter writeLog ("UPDATE: " + ((task TaskPackage) getOrElse "") + " --> " + progress + "%", Level.INFO)
+					DbControl.updateTask(task)
+				}
+			}
+		}
+		// DEBUG
+		LogWriter writeLog("End-Of-Method", Level.DEBUG)
+	}
+
+	class AriaThread(port: Int, uri: String, gid: String, /*owner: String, restarting: Boolean,*/ isHttp: Boolean) extends Runnable {
 		private var _httpUsername: Option[String] = None
 		private var _httpPassword: Option[String] = None
 
 		def setCredentials(username: String, password: String) {
 			_httpUsername = Some(username)
 			_httpPassword = Some(password)
+		}
+
+		private var _executor: Option[DefaultExecutor] = None
+
+		def getExecutor: DefaultExecutor = {
+			_executor.getOrElse(null)
 		}
 
 		override def run() {
@@ -154,6 +234,7 @@ class AriaProcessor {
 			val watchdog = new ExecuteWatchdog(ExecuteWatchdog INFINITE_TIMEOUT)
 			val executor = new DefaultExecutor
 			executor setWatchdog watchdog
+			_executor = Some(executor)
 			executor execute cmdLine
 
 			/*val taskPid: Int = {
@@ -180,7 +261,7 @@ class AriaProcessor {
 				line = br readLine()
 			}*/
 
-			if (!restarting) {
+			/*if (!restarting) {
 				// DEBUG
 				LogWriter writeLog ("Adding new task to DB", Level.DEBUG)
 				DbControl.addTask(new Task {
@@ -243,7 +324,7 @@ class AriaProcessor {
 				}
 			}
 			// DEBUG
-			LogWriter writeLog("End-Of-Method", Level.DEBUG)
+			LogWriter writeLog("End-Of-Method", Level.DEBUG)*/
 			//process.waitFor()
 		}
 	}
