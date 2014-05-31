41b1295b55f22d704936c549b8f810c6c8f6da70
viper <r.kurniawan@fluxodesign.net>
2014-06-01 08:07:51 +1000

    * PluginProcessor.scala: added license text and class documentation; removed getPluginManagerUtil() * TrTPB.scala: added license text and class documentation

--- src/main/scala/net/fluxo/plugins/tpb/TrTPB.scala
+++ src/main/scala/net/fluxo/plugins/tpb/TrTPB.scala
@@ -1,11 +1,34 @@
+/*
+ * TrTPB.scala
+ *
+ * Copyright (c) 2014 Ronald Kurniawan. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
 package net.fluxo.plugins.tpb
 
 import net.xeoh.plugins.base.Plugin
 import org.apache.log4j.Level
 
 /**
- * Created by viper 
- * Time: 31/05/14.
+ * The trait (interface) that defines the methods used for processing TPB requests. This trait is directly
+ * borrowed from the plugin. Part of the DownloadDaemon plugin framework.
+ *
+ * @author Ronald Kurniawan (viper)
+ * @version 30/05/14.
  */
 trait TrTPB extends Plugin {
 
