fd6eff19c352b6974f65fb09f69b92e9fef74481
Ronald Kurniawan <r.kurniawan@fluxodesign.net>
2015-08-18 21:32:51 +1000

    * FluxoWSProcess.java: removed commented code

--- src/main/scala/net/fluxo/dd/FluxoWSProcess.java
+++ src/main/scala/net/fluxo/dd/FluxoWSProcess.java
@@ -554,46 +554,4 @@ public class FluxoWSProcess {
 		}
 	}
 
-	/*private class ADTObject {
-		String owner;
-		String originalGid;
-		String activeGid;
-		long totalLength = -1;
-		long completedLength = -1;
-		String packageName;
-		String infoHash;
-
-		public String getOwner() {
-			return owner;
-		}
-
-		public String getOriginalGid() {
-			return originalGid;
-		}
-
-		public String getActiveGid() {
-			return activeGid;
-		}
-
-		public long getTotalLength() {
-			return totalLength;
-		}
-
-		public long getCompletedLength() {
-			return completedLength;
-		}
-
-		public String getPackageName() {
-			return packageName;
-		}
-
-		public String getInfoHash() {
-			return infoHash;
-		}
-
-		public boolean isOk() {
-			return !getOwner().isEmpty() &&!getOriginalGid().isEmpty() && !getActiveGid().isEmpty() && getTotalLength() >= 0
-				&& getCompletedLength() >= 0 &&	!getPackageName().isEmpty() && !getInfoHash().isEmpty();
-		}
-	}*/
 }
