fba45427e2867f25439f9d81e749535db1b33edf
viper <ronald.kurniawan@gmail.com>
2015-12-23 21:51:45 +1000

    * HttpDaemon.scala: modified the list of cipher suites to fix the ephemeral problem with some browsers

--- src/main/scala/net/fluxo/dd/HttpDaemon.scala
+++ src/main/scala/net/fluxo/dd/HttpDaemon.scala
@@ -91,7 +91,13 @@ class HttpDaemon(port: Int, sslPort: Int) extends Runnable {
 		sslContextFactory setKeyManagerPassword OUtils.readConfig.SSLKeymanagerPassword.getOrElse("")
 		sslContextFactory addExcludeProtocols "SSLv3"
 		sslContextFactory addExcludeProtocols "SSLv2Hello"
-		sslContextFactory setIncludeCipherSuites "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256"
+		sslContextFactory setIncludeCipherSuites("TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256",
+			"TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
+			"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256",
+			"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA",
+			"TLS_DHE_RSA_WITH_AES_128_CBC_SHA",
+			"TLS_RSA_WITH_AES_128_CBC_SHA",
+			"TLS_DHE_DSS_WITH_AES_128_CBC_SHA")
 		val httpsConfig = new HttpConfiguration()
 		httpsConfig setSecureScheme "https"
 		httpsConfig setSecurePort sslPort
